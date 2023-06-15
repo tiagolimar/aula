66b94dbde42d8e343b55f1f3cdfa0608561e077d
tiagolimar <tiagolima.r2@gmail.com>
2023-06-15 20:40:34 -0300

    conclusão do exercício do CEP

--- execs/cep/index.js
+++ execs/cep/index.js
@@ -7,13 +7,16 @@
 // Municípios por UF
 // https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
 
-
-
-let consultar_cep = ()=>{
+const consultar_cep = ()=>{
     fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
     .then(response=>response.json())
     .then(json=>{
-        console.log(json);
+        logradouro.value = json['logradouro']
+        complemento.value = json['complemento']
+        bairro.value = json['bairro']
+        numero.focus()
+        estados.value = json['uf']
+        consultar_cidades(json['localidade'])
     })
 }
 
@@ -21,19 +24,22 @@ let consultar_estados = ()=>{
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
     .then(response=>response.json())
     .then(json=>{
+        estados.innerHTML =  '<option>Escolher...</option>'
         json.forEach(element => {
             estados.innerHTML += `<option value=${element.sigla}>${element.nome}</option>`
         });
+        consultar_cidades();
     })
 }
 
-let consultar_cidades = ()=>{
-    console.log('foi');
+let consultar_cidades = (nome='')=>{
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estados.value}/municipios`)
         .then(response=>response.json())
         .then(json=>{
+            cidades.innerHTML = '<option>Escolher...</option>'
             json.forEach(element=>{
-                cidades.innerHTML += `<option>${element.nome}</option>`
+                cidades.innerHTML += `<option value="${element.nome}">${element.nome}</option>`
+                if (nome) cidades.value = nome
             })
         });
 }
